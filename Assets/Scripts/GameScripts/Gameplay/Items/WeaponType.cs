75fc55cb9762a47c63dbc0a8937e23a783354471
TomTrenki <tomtrenki23@gmail.com>
2013-06-01 23:43:16 +0200

    ItemType, WeaponType
    basic classes and test prefabs

---
+++ Assets/Scripts/GameScripts/Gameplay/Items/WeaponType.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+
+public class WeaponType : ItemType {
+	public float cooldown;
+	public Projectile projectilePrefab;
+	public float projectileVelocity;
+}
